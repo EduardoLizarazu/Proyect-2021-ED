08a36ce79c490e4cc2beafcb4dde10f70134bf41
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2021-12-04 15:26:06 -0400

    login added

---
+++ JavaScript/login.js
@@ -0,0 +1,8 @@
+
+const getBackArrowIcon = document.getElementById("backArrow")
+// GET LOGIN FORM   
+const getformLogin = document.getElementById("login-form");
+
+const element = document.getElementById("login-form");
+const child = document.getElementById("adding");
+element.insertBefore(getBackArrowIcon, child)
