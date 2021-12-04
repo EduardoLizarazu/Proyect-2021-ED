3959ed3e8958efac642eed056f4792a54377af6e
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2021-12-04 13:54:39 -0400

    scale and hidden button start on js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -13,8 +13,11 @@ getMain.style.height = `${heightWindow - getHeaderHeightNum}px`;
  
 
 const getButtonStart = document.getElementById("button-start");
-getButtonStart.onclick = displayBlur;
-function displayBlur() {
+
+// DISPLAY BLUR IMG INDEX
+getButtonStart.addEventListener("click", displayBlurImgIndex)
+function displayBlurImgIndex() {
+    console.log("displayBlur");
     const getImgBg = document.getElementById("bg-img--index");
     let id = null;
     let pos = 0;
@@ -28,5 +31,29 @@ function displayBlur() {
             getImgBg.style.filter = `blur(${pos}px)`;
         }
     }
+}
+
+// SCALE BUTTON START
+getButtonStart.addEventListener("click", displayScaleButtonStart);
+function displayScaleButtonStart () {
+    console.log("displayScaleButtonStart")
+    let id = null;
+    let pos = 0;
+    id = setInterval(frame, 5);
+    function frame () {
+        if(pos == .5) {
+            clearInterval(id);
+        } else {
+            pos += .1
+            getButtonStart.style.transform = `scale(${pos})`
+        }
+    }
+}
+
+// HIDDEN BUTTON
+getButtonStart.addEventListener("click", hiddenButtonStart);
+function hiddenButtonStart () {
+    console.log("hiddenButon");
+    getButtonStart.style.visibility = "hidden";
 }
 
