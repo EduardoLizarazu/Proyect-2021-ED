71c2fa35afaab237a0c74c16245a598015958774
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2021-12-04 12:14:38 -0400

    added js animate blur to img

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -12,4 +12,21 @@ const getMain = document.getElementById("main");
 getMain.style.height = `${heightWindow - getHeaderHeightNum}px`;
  
 
+const getButtonStart = document.getElementById("button-start");
+getButtonStart.onclick = displayBlur;
+function displayBlur() {
+    const getImgBg = document.getElementById("bg-img--index");
+    let id = null;
+    let pos = 0;
+    // clearInterval(id);
+    id = setInterval(frame, 50);
+    function frame () {
+        if (pos == 10) {
+            clearInterval(id);
+        } else {
+            pos++;
+            getImgBg.style.filter = `blur(${pos}px)`;
+        }
+    }
+}
 
